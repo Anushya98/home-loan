9d541f389f5ff2dd124b952615e3d0d6cc4294a1
Anushya98 <130074598+Anushya98@users.noreply.github.com>
2024-03-27 12:19:13 +0530

    commit

--- app/calculator/page.js
+++ app/calculator/page.js
@@ -27,25 +27,30 @@ export default function LoanCalculator() {
 
     const handleAmountChange = (event) => {
         setAmount(event.target.value);
+        calculateLoan(event.target.value, interest, time);
     };
 
     const handleAmountDrag = (event) => {
         setAmount(parseInt(event.target.value));
+        calculateLoan(parseInt(event.target.value), interest, time);
     };
 
     const handlePrincipalchange = (event) => {
         setAmount(event.target.value);
+        calculateLoan(event.target.value, interest, time);
     };
 
     const handleInterestchange = (event) => {
         setInterest(event.target.value);
+        calculateLoan(amount, event.target.value, time);
     };
 
     const handleTimeChange = (event) => {
         setTime(event.target.value);
+        calculateLoan(amount, interest, event.target.value);
     };
 
-    const calculateLoan = () => {
+    const calculateLoan = (amount, interest, time) => {
         if (amount > 0 && interest > 0 && time > 0) {
             let p = parseFloat(amount);
             let r = parseFloat(interest);
@@ -61,25 +66,15 @@ export default function LoanCalculator() {
             setEmi(Math.round(calcemi));
             setTotalinterest(Math.round(calcemi * n - p));
             setTotalpayment(Math.round(calcemi * n));
-        } else {
-            toast.error("Amount, Interest, and Period must be greater than 0!", {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: true,
-                toastId: "id",
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-            });
         }
     };
+
     const data = {
         labels: ['Principal', 'Interest'],
         datasets: [
             {
                 label: 'Payment Breakdown',
-                data: [amount, totalinterest],
+                data: [totalpayment, totalinterest],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.6)',
                     'rgba(54, 162, 235, 0.6)',
@@ -92,127 +87,96 @@ export default function LoanCalculator() {
             <ToastContainer />
             <Box sx={{ m: 10 }} />
             <Container>
-                <Card>
-                    <div style={{ overflowY: "auto", padding: "40px" }}>
-                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <Typography variant="h5">Amount</Typography>
-                            <Typography variant="subtitle1">₹ {amount}</Typography>
-                        </Box>
-                        <Box sx={{ m: 2 }} />
-                        <input
-                            type="range"
-                            min="10000"
-                            max="500000"
-                            step="10000"
-                            value={amount}
-                            onChange={handleAmountDrag}
-                            style={{ width: '100%' }}
-                        />
-                        <Typography variant="caption" display="block" gutterBottom>
-                            ₹ 10,000
-                        </Typography>
-                        <Typography variant="caption" display="block" gutterBottom style={{ textAlign: 'right' }}>
-                            ₹ 500,000
-                        </Typography>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: '20px', marginTop: '20px' }}>
+                    <Card sx={{ flex: 1 }}>
+                        <div style={{ overflowY: "auto", padding: "40px" }}>
+                            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Typography variant="h5">Amount</Typography>
+                                <Typography variant="subtitle1">₹ {amount}</Typography>
+                            </Box>
+                            <Box sx={{ m: 2 }} />
+                            <input
+                                type="range"
+                                min="10000"
+                                max="500000"
+                                step="10000"
+                                value={amount}
+                                onChange={handleAmountDrag}
+                                style={{ width: '100%' }}
+                            />
+                            <Typography variant="caption" display="block" gutterBottom>
+                                ₹ 10,000
+                            </Typography>
+                            <Typography variant="caption" display="block" gutterBottom style={{ textAlign: 'right' }}>
+                                ₹ 500,000
+                            </Typography>
 
-                        <Box sx={{ m: 2 }} />
-                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <Typography variant="h5">Loan Duration (Years)</Typography>
-                            <Typography variant="subtitle1">{time} years</Typography>
-                        </Box>
-                        <Box sx={{ m: 2 }} />
-                        <input
-                            type="range"
-                            min="5"
-                            max="20"
-                            step="1"
-                            value={time}
-                            onChange={handleTimeChange}
-                            style={{ width: '100%' }}
-                        />
-                        <Typography variant="caption" display="block" gutterBottom>
-                            5 years
-                        </Typography>
-                        <Typography variant="caption" display="block" gutterBottom style={{ textAlign: 'right' }}>
-                            20 years
-                        </Typography>
+                            <Box sx={{ m: 2 }} />
+                            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Typography variant="h5">Loan Duration (Years)</Typography>
+                                <Typography variant="subtitle1">{time} years</Typography>
+                            </Box>
+                            <Box sx={{ m: 2 }} />
+                            <input
+                                type="range"
+                                min="5"
+                                max="20"
+                                step="1"
+                                value={time}
+                                onChange={handleTimeChange}
+                                style={{ width: '100%' }}
+                            />
+                            <Typography variant="caption" display="block" gutterBottom>
+                                5 years
+                            </Typography>
+                            <Typography variant="caption" display="block" gutterBottom style={{ textAlign: 'right' }}>
+                                20 years
+                            </Typography>
 
-                        <Box sx={{ m: 2 }} />
-                        <TextField
-                            label="Enter interest rate"
-                            variant="outlined"
-                            type="number"
-                            name="interest_rate"
-                            onChange={handleInterestchange}
-                            value={interest}
-                            sx={{ minWidth: "92%" }}
-                        />
-                        <Box
-                            m={1}
-                            display="flex"
-                            justifyContent="center"
-                            alignItems="center"
-                        >
-                            <FormControl
-                                size="large"
-                                align="center"
-                                style={{ marginTop: "10px" }}
-                            >
-                                <Button
-                                    variant="outlined"
-                                    size="large"
-                                    onClick={calculateLoan}
-                                >
-                                    Calculate
-                                </Button>
-                            </FormControl>
-                        </Box>
-                    </div>
-                </Card>
-                <Box sx={{ m: 4 }} />
-                <Card>
-                    <TableContainer>
-                        <Table>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell align="center">
-                                        <Typography variant="p">Loan EMI</Typography>
-                                        <Box sx={{ m: 1 }} />
-                                        <Typography variant="h6" className="font-bold">
-                                            ₹ {emi}
-                                        </Typography>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Typography variant="p">
-                                            Total Interest Payable
-                                        </Typography>
-                                        <Box sx={{ m: 1 }} />
-                                        <Typography variant="h6" className="font-bold">
-                                            ₹ {totalinterest}
-                                        </Typography>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <Typography variant="p">
-                                            Total Payment (Principal + Interest)
-                                        </Typography>
-                                        <Box sx={{ m: 1 }} />
-                                        <Typography variant="h6" className="font-bold">
-                                            ₹ {totalpayment}
-                                        </Typography>
-                                    </TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Card>
-                <Card sx={{ padding: "40px", marginBottom: "20px" }}>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <Typography variant="h5" align="center">Payment Breakdown</Typography>
-                        <Box width={300} height={300}>
-                            <Doughnut data={data} />
-                        </Box>
-                    </Box>
-                </Card>
+                            <Box sx={{ m: 2 }} />
+                            <TextField
+                                label="Enter interest rate"
+                                variant="outlined"
+                                type="number"
+                                name="interest_rate"
+                                onChange={handleInterestchange}
+                                value={interest}
+                                sx={{ minWidth: "92%" }}
+                            />
+                        </div>
+                    </Card>
+                    <Card sx={{ flex: 1 }}>
+                        <TableContainer>
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+                                    <Typography variant="h5">Total Pays</Typography>
+                                </Box>
+                                <Table>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell align="center">
+                                                <Typography variant="h6" className="font-bold">Loan EMI: ₹ {emi}</Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align="center">
+                                                <Typography variant="h6" className="font-bold">Total Interest Payable : ₹ {totalinterest}</Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align="center">
+                                                <Typography variant="h6" className="font-bold">Total Payment (Principal + Interest) : ₹ {totalpayment}</Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                                <Box width={200} height={200} sx={{ marginTop: '20px', marginBottom: "20px" }}>
+                                    <Doughnut data={data} />
+                                </Box>
+                            </Box>
+                        </TableContainer>
+                    </Card>
+                </Box>
             </Container>
         </>
     );
